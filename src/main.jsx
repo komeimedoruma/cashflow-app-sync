e6cee936c9adaea582688a96e01bce9b8a94e801
komeimedoruma <m.koumei12@gmail.com>
2026-04-19 13:37:19 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CashflowApp from './CashflowApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CashflowApp />
+  </React.StrictMode>,
+)
